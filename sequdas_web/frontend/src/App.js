db50317e252964e74b8e5b9663fc76db3488ad26
Dan Fornika <dfornika@gmail.com>
2018-02-01 14:39:27 -0800

    Cleaning up unused components

--- sequdas_web/frontend/src/App.js
+++ sequdas_web/frontend/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
+import { BrowserRouter } from 'react-router-dom'
 
-import SequenceRunTable from './components/SequenceRunTable.jsx'
+import Header from './components/Header.jsx';
+import Nav from './components/Nav.jsx';
+import SequenceRunTable from './components/SequenceRunTable.jsx';
+
 class App extends Component {
     constructor(props) {
 	super(props);	
@@ -11,12 +12,13 @@ class App extends Component {
     
     render() {
 	return (
-		<div className="App">
-		<Header />
-		<BrowserRouter>
+	    <div className="App">
+	      <Header />
+	      <Nav />
+	      <BrowserRouter>
 		<Route exact path='/' component={SequenceRunTable} onEnter={requireAuth} />
 		<Route exact path='/login/' component={Login} />
-		</BrowserRouter>
+	      </BrowserRouter>
 	    </div>
 	);
     }
---
+++ sequdas_web/frontend/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Nav.css';
+
+class Header extends Component {
+ 
+    render() {
+	return (
+	    <ul>
+	    </ul>
+	);
+    }
+}
+
+export default Nav;
